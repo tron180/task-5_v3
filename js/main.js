f5bbe00d3a6a1dce049bf55a9c2f978fe79a27f8
Yash Mishra <yash.mishra@raweng.com>
2019-05-23 18:53:32 +0530

    added basic functionalities

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
+
+// + operator
 function inc(){
     var value = document.getElementById('inp').value;
 	value++;
     document.getElementById('inp').value = value;
 }
 
+// - operator
 function dec(){
     var value = document.getElementById('inp').value;
     value--;
     document.getElementById('inp').value = value;
 }
 
+// Add to cart button
 function cart(){
     var value = document.getElementById('inp').value;
     document.getElementById('num').innerHTML = value;
+    document.getElementById('num1').innerHTML = value;
 }
 
+// Selected Effects
+var cont = document.getElementById("sticky");
+var menu = cont.getElementsByClassName("py");
+for (var i = 0; i < menu.length; i++) {
+menu[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("selecting");
+    current[0].className = current[0].className.replace(" selecting", "");
+    this.className += " selecting";
+    });
+}
+
+
+// Stars effect
 var count;
 function starmark(item){
     count=item.id[0];
@@ -26,7 +44,7 @@ function starmark(item){
         }
         else
         {
-           document.getElementById((i+1)+subid).style.color="black";
+           document.getElementById((i+1)+subid).style.color="#e6e6e6";
         }
     }
 }
