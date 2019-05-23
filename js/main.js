257965288fb1d451292ebf5158229d3bb5a30e25
Yash Mishra <yash.mishra@raweng.com>
2019-05-23 13:13:24 +0530

    bugs fixed

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ function cart(){
     var value = document.getElementById('inp').value;
     document.getElementById('num').innerHTML = value;
 }
+
+var count;
+function starmark(item){
+    count=item.id[0];
+    sessionStorage.starRating = count;
+    var subid= item.id.substring(1);
+    for(var i=0;i<5;i++){
+        if(i<count){
+            document.getElementById((i+1)+subid).style.color="#ffc107";
+        }
+        else
+        {
+           document.getElementById((i+1)+subid).style.color="black";
+        }
+    }
+}
